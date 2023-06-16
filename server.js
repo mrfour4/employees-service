64d3cd2cc0f861ac20d88e67bbabc9885463065c
mrfour4 <21521609@gm.uit.edu.vn>
2023-06-16 16:04:09 +0700

    del logger

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ const PORT = process.env.PORT || 3500;
 // Connect to MongoDB
 connectDB();
 
-// custom middleware logger
-app.use(logger);
-
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 app.use(credentials);
